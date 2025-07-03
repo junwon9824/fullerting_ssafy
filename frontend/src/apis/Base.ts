7c7b3b634955e6fd5fd7e5ee38d2278f8b090e85
junwon <junwon1131@naver.com>
2025-07-03 13:50:14 +0900

    env modify

--- frontend/src/apis/Base.ts
+++ frontend/src/apis/Base.ts
@@ -1,14 +1,14 @@
 import axios from "axios";
 
 // const baseURL = import.meta.env.VITE_SERVER_URL;
-const baseURL = import.meta.env.VITE_REACT_APP_API_URL
+// const baseURL = import.meta.env.VITE_REACT_APP_API_URL
 
 // const baseURL = "http://localhost:5000/service/v1";
 // const loginURL = "http://localhost:5000/public/v1";
 
 // API 인스턴스 생성
 export const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL ,
+  baseURL: import.meta.env.VITE_REACT_APP_API_URL ,
   // baseURL: import.meta.env.VITE_API_URL || "https://j10c102.p.ssafy.io/api",
   withCredentials: true,
 });
